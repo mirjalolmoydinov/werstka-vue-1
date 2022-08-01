ba6abc1394d8e55993ae66ab2ed68edebfd362b2
HRMxan <hrmxan@gmail.com>
2022-08-01 17:09:47 +0500

    fixed route

--- src/views/KakPokupka.vue
+++ src/views/KakPokupka.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="kakpokubka">
-    <MenuVie></MenuVie>
     <BannerView></BannerView>
     <SliderView></SliderView>
     <ProdokView></ProdokView>
@@ -15,23 +14,20 @@
   </div>
 </template>
 <script>
-import MenuVie from '@/components/MenuVie.vue'
-import BannerView from '@/components/menu_1_components/BannerView.vue'
-import SliderView from '@/components/menu_1_components/SliderView.vue'
-import ProdokView from '@/components/menu_1_components/ProdokView.vue'
-import ContentView from '../components/menu_1_components/ContentView.vue'
-import TowTextView from '../components/menu_1_components/TowTextView.vue'
-import FormaView from '@/components/menu_1_components/FormaView.vue'
-import VariantDostavkaVie from '@/components/menu_1_components/VariantDostavkaVie.vue'
-import ByukBritanyaVie from '../components/menu_1_components/ByukBritanyaVie.vue'
-import VirtualVie from '@/components/menu_1_components/VirtualVie.vue'
-import TowText from '@/components/menu_1_components/TowText.vue'
-import SriCardVie from '@/components/menu_1_components/SriCardVie.vue'
-
+import BannerView from "@/components/menu_1_components/BannerView.vue";
+import SliderView from "@/components/menu_1_components/SliderView.vue";
+import ProdokView from "@/components/menu_1_components/ProdokView.vue";
+import ContentView from "../components/menu_1_components/ContentView.vue";
+import TowTextView from "../components/menu_1_components/TowTextView.vue";
+import FormaView from "@/components/menu_1_components/FormaView.vue";
+import VariantDostavkaVie from "@/components/menu_1_components/VariantDostavkaVie.vue";
+import ByukBritanyaVie from "../components/menu_1_components/ByukBritanyaVie.vue";
+import VirtualVie from "@/components/menu_1_components/VirtualVie.vue";
+import TowText from "@/components/menu_1_components/TowText.vue";
+import SriCardVie from "@/components/menu_1_components/SriCardVie.vue";
 
 export default {
   components: {
-    MenuVie,
     BannerView,
     SliderView,
     ProdokView,
@@ -44,7 +40,7 @@ export default {
     TowText,
     SriCardVie,
   },
-}
+};
 </script>
 
 <style>
--- src/views/RasprodajaTovar.vue
+++ src/views/RasprodajaTovar.vue
@@ -1,21 +1,13 @@
 <template>
   <div class="rasprodaja">
-    <menu-vie></menu-vie>
     <h1>raspradaja</h1>
-
-
-
   </div>
 </template>
 
 <script>
-import MenuVie from '@/components/MenuVie.vue'
-
 export default {
-components:{
-  MenuVie,
-} 
-}
+  components: {},
+};
 </script>
 
 MenuVie
