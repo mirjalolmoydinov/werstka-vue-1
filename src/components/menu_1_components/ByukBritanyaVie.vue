f50bacbf7c5137273e2028d15f85f231b81190fd
HRMxan <hrmxan@gmail.com>
2022-08-01 13:24:33 +0500

    hrmxan fix

--- src/components/menu_1_components/ByukBritanyaVie.vue
+++ src/components/menu_1_components/ByukBritanyaVie.vue
@@ -2,42 +2,62 @@
   <div id="byukbritanya">
     <b-container>
       <div class="container_text">
-        <p>4 простых шага для ваших покупок в Великобритании </p>
+        <p>4 простых шага для ваших покупок в Великобритании</p>
       </div>
       <div class="container_card">
-          <div>
-            <b-card-group deck>
-              <b-card title="вы ВЫБИРАЕТЕ ТОВАР" img-src="@assets/img/britan_1.svg" img-alt="Image"
-                img-top>
-                <b-card-text>
-                  в английском магазине, оплачиваете и указываете адрес склада OkeyPost
-                </b-card-text>
-              </b-card>
+        <div>
+          <b-card-group deck>
+            <b-card
+              title="вы ВЫБИРАЕТЕ ТОВАР"
+              :img-src="require('../../assets/img/britan_1.svg')"
+              img-alt="Image"
+              img-top
+            >
+              <b-card-text>
+                в английском магазине, оплачиваете и указываете адрес склада
+                OkeyPost
+              </b-card-text>
+            </b-card>
 
-              <b-card title="ПОКУПКИ ПРИХОДЯТ НА СКЛАД" img-src="@assets/img/britan_1.svg"
-                img-alt="Image" img-top>
-                <b-card-text>
-                  и появляются в вашем личном кабинете на нашем сайте
-                </b-card-text>
-              </b-card>
+            <b-card
+              title="ПОКУПКИ ПРИХОДЯТ НА СКЛАД"
+              :img-src="require('../../assets/img/britan_2.svg')"
+              img-alt="Image"
+              img-top
+            >
+              <b-card-text>
+                и появляются в вашем личном кабинете на нашем сайте
+              </b-card-text>
+            </b-card>
 
-              <b-card title="ОТПРАВЛЯЕМ ИХ К ВАМ ДОМОЙ" img-src="@assets/img/britan_1.svg"
-                img-alt="Image" img-top>
-                <b-card-text>
-                  в надежной упаковке, по выгодной цене и очень быстро
-                </b-card-text>
-              </b-card>
-              <b-card title="ЗАБИРАЕТЕ свою ПОСЫЛКУ" img-src="@assets/img/britan_1.svg" img-alt="Image"
-                img-top>
-                <b-card-text>
-                  в пункте выдачи, на почте или с курьером
-                </b-card-text>
-              </b-card>
-            </b-card-group>
-          </div>
+            <b-card
+              title="ОТПРАВЛЯЕМ ИХ К ВАМ ДОМОЙ"
+              :img-src="require('../../assets/img/britan_1.svg')"
+              img-alt="Image"
+              img-top
+            >
+              <b-card-text>
+                в надежной упаковке, по выгодной цене и очень быстро
+              </b-card-text>
+            </b-card>
+            <b-card
+              title="ЗАБИРАЕТЕ свою ПОСЫЛКУ"
+              :img-src="require('../../assets/img/britan_1.svg')"
+              img-alt="Image"
+              img-top
+            >
+              <b-card-text>
+                в пункте выдачи, на почте или с курьером
+              </b-card-text>
+            </b-card>
+          </b-card-group>
+        </div>
       </div>
       <div class="container_text_2">
-        <p>Не хотите разбираться самостоятельно, наши мастера шопинга с радостью купят все товары за вас.</p>
+        <p>
+          Не хотите разбираться самостоятельно, наши мастера шопинга с радостью
+          купят все товары за вас.
+        </p>
       </div>
       <div class="container_btun">
         <b-button>Быстрая покупка</b-button>
@@ -48,15 +68,15 @@
 
 <script>
 export default {
-data() {
-  return {
-    kol:'../../assets/img/britan_1.svg'
-  }
-},
-}
+  data() {
+    return {
+      kol: "../../assets/img/britan_1.svg",
+    };
+  },
+};
 </script>
 
-<style scoped>
+<style>
 #byukbritanya {
   width: 1353px;
   height: 755px;
@@ -65,12 +85,12 @@ data() {
 }
 
 .container_text p {
-  font-family: 'Gilroy';
+  font-family: "Gilroy";
   font-style: normal;
   font-weight: 800;
   font-size: 44px;
   line-height: 56px;
-  color: #1F2428;
+  color: #1f2428;
   width: 695px;
   height: 112px;
 }
@@ -78,18 +98,18 @@ data() {
 .container_text_2 {
   width: 760px;
   height: 72px;
-  font-family: 'Gilroy';
+  font-family: "Gilroy";
   font-style: normal;
   font-weight: 300;
   font-size: 20px;
   line-height: 36px;
-  color: #1F2428;
+  color: #1f2428;
 }
 
 .container_btun button {
   width: 240px;
   height: 66px;
-  background: linear-gradient(90deg, #00C97B 0%, #00D981 96.96%);
+  background: linear-gradient(90deg, #00c97b 0%, #00d981 96.96%);
   box-shadow: 0px 4px 4px rgba(1, 203, 124, 0.2);
   border-radius: 36px;
   border: none;
